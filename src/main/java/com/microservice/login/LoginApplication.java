1739552c66fab44f894b474d7f441e5111b37857
thiagoAmorim2 <thiago.amorim_2@live.com>
2023-05-09 12:34:39 -0300

    implementação da documentação (apiDocs + Swagger)

--- src/main/java/com/microservice/login/LoginApplication.java
+++ src/main/java/com/microservice/login/LoginApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(
+info = @Info(title = "Api de Pessoas", version = "3.0.1", description = "Api"),
+servers = {@Server(url = "http://localhost:8080")}
+)
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class LoginApplication {
 
